6b1c930e7a6abd5e19d3389ae3fadfbe39eccd9e
sourav saha <souravsaha675@gmail.com>
2021-03-29 02:46:32 +0530

    Login & Contacts model

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,18 @@ const config = require("config");
 
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("gaefsef");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 router.post(
