d369051fc05d57699bc42909fc59e31c1306481c
sourav saha <souravsaha675@gmail.com>
2021-04-16 21:44:16 +0530

    Alert Component & context

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,34 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alert";
 
 import ContactState from "./context/contact/contactState";
 import AuthState from "./context/auth/AuthState";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/signup" component={Register} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/about" component={About} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/signup" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/about" component={About} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -19,7 +24,13 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Register submit");
+    if (name === "" || password === "" || email === "") {
+      setAlert("Please enter all fields", "danger");
+    } else if (password !== confirm_password) {
+      setAlert("Passwords do not match", "danger");
+    } else {
+      console.log("Register submit");
+    }
   };
 
   return (
@@ -34,7 +45,13 @@ const Register = () => {
         </div>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
@@ -43,6 +60,8 @@ const Register = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -52,6 +71,8 @@ const Register = () => {
             name="confirm_password"
             value={confirm_password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <input
